7abd94e029905eb89745706ecf0cded38fedd104
lucas04501 <lucaspds9@hotmail.com>
2026-04-30 12:59:04 -0300

    class_user

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
 
 # definindo o modelo de dados para os produtos. no meu produto tem (id, name, price e description)
 class Product(db.Model):
